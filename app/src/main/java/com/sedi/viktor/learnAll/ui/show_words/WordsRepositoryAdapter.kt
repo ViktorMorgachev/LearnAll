0b5ccb990027f6ed6950aeafa9459b1c6d1faf4b
Viktor <morgachev.v.s@mail.ru>
2019-11-18 14:18:25 +0600

    Changed logic to Data Binding in UI

--- app/src/main/java/com/sedi/viktor/learnAll/ui/show_words/WordsRepositoryAdapter.kt
+++ app/src/main/java/com/sedi/viktor/learnAll/ui/show_words/WordsRepositoryAdapter.kt
@@ -3,47 +3,33 @@ package com.sedi.viktor.learnAll.ui.show_words
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.sedi.viktor.learnAll.R
 import com.sedi.viktor.learnAll.databinding.ItemWordLayoutBinding
 import com.sedi.viktor.learnAll.models.WordItem
 
-class WordsRepositoryAdapter(val items: List<WordItem>) :
+class WordsRepositoryAdapter(private val items: List<WordItem>) :
     RecyclerView.Adapter<WordsRepositoryAdapter.WordsHolder>() {
 
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ) = WordsHolder(
-        LayoutInflater.from(parent.context).inflate(
-            R.layout.item_word_layout,
-            parent,
-            false
-        )
-    )
+    ): WordsHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemWordLayoutBinding.inflate(inflater, parent, false)
+        return WordsHolder(binding.root)
+    }
+
 
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: WordsHolder, position: Int) {
-        holder.bind(items[position])
+        holder.binding!!.word = items[position]
     }
 
     inner class WordsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val tvOther = itemView.findViewById<TextView>(R.id.tv_other_name)
-        val tvNativeName = itemView.findViewById<TextView>(R.id.tv_native_name)
-        lateinit var  binding: ItemWordLayoutBinding
-
-        fun bind(item: WordItem) {
-
-            tvOther.text = item.otherName
-            tvNativeName.text = item.nativeName
-
-        }
-
+        var binding = DataBindingUtil.bind<ItemWordLayoutBinding>(itemView)
     }
 
 }
