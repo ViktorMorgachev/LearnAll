886e6281472ec4c0f90350b0f976f1033dc1e8c0
Viktor <morgachev.v.s@mail.ru>
2019-11-21 11:26:24 +0600

    Добавил детекторы работы с экраном

--- app/src/main/java/com/sedi/viktor/learnAll/ui/scan_words/CaptureActivity.kt
+++ app/src/main/java/com/sedi/viktor/learnAll/ui/scan_words/CaptureActivity.kt
@@ -3,6 +3,9 @@ package com.sedi.viktor.learnAll.ui.scan_words
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.sedi.viktor.learnAll.R
@@ -13,6 +16,8 @@ class CaptureActivity : AppCompatActivity() {
 
     lateinit var cameraSourcePreview: CameraSourcePreview
     lateinit var graphicOverlay: GraphicOverlay<OcrGraphic>
+    lateinit var gestureDetector: GestureDetector
+    lateinit var scaleDetector: ScaleGestureDetector
 
     // Permission request codes need to be < 256
     private val RC_HANDLE_CAMERA_PERMISSION = 2
@@ -32,6 +37,9 @@ class CaptureActivity : AppCompatActivity() {
             requestCameraPermission();
         }
 
+        gestureDetector = GestureDetector(this, CaptureGestureListener())
+        scaleDetector = ScaleGestureDetector(this, ScaleDetectorListener())
+
     }
 
     private fun requestCameraPermission() {
@@ -42,4 +50,53 @@ class CaptureActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        val b = scaleDetector.onTouchEvent(event)
+
+        val c = gestureDetector.onTouchEvent(event)
+
+        return super.onTouchEvent(event)
+    }
+
+    /**
+     * onTap is called to speak the tapped TextBlock, if any, out loud.
+     *
+     * @param rawX - the raw position of the tap
+     * @param rawY - the raw position of the tap.
+     * @return true if the tap was on a TextBlock
+     */
+    private fun onTap(rawX: Float, rawY: Float): Boolean {
+        // TODO: Speak the text when the user taps on screen.
+        return false
+    }
+
+
+    private inner class CaptureGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+            return onTap(e.rawX, e.rawY) || super.onSingleTapConfirmed(e)
+        }
+    }
+
+
+    private inner class ScaleDetectorListener : ScaleGestureDetector.OnScaleGestureListener {
+
+
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
+            return true
+        }
+
+        override fun onScaleEnd(detector: ScaleGestureDetector) {
+            // if (cameraSource != null) {
+            //   cameraSource.doZoom(detector.scaleFactor)
+            // }
+        }
+    }
+
 }
