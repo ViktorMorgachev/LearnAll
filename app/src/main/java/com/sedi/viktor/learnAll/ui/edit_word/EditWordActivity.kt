77dd31f0555d33144660c15b00c0607fc5ab8a21
Viktor <morgachev.v.s@mail.ru>
2019-12-01 14:38:28 +0600

    Доработал дизайн, работаю над автоматическим переводом после ввода слова

---
+++ app/src/main/java/com/sedi/viktor/learnAll/ui/edit_word/EditWordActivity.kt
@@ -0,0 +1,22 @@
+package com.sedi.viktor.learnAll.ui.edit_word
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sedi.viktor.learnAll.R
+
+class EditWordActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.word_edit_layout_activity)
+
+
+    }
+
+    fun SaveWord(view: View) {
+
+    }
+}
